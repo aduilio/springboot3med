3d816b76de6befa4a74501c9c281ecd9f307cf65
Aduilio Silva <developer.aduilio@gmail.com>
2023-02-20 13:30:57 -0300

    Get only active doctors
    - Create a method to return only active doctors.
      JPA repository recognize the method name `byActiveTrue` and
      create the query to return only the entities with active = true.

--- src/main/java/com/aduilio/med/repository/DoctorRepository.java
+++ src/main/java/com/aduilio/med/repository/DoctorRepository.java
@@ -1,5 +1,7 @@
 package com.aduilio.med.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.aduilio.med.entity.Doctor;
@@ -8,5 +10,13 @@ import com.aduilio.med.entity.Doctor;
  * Provides the methods to persist a Doctor.
  */
 public interface DoctorRepository extends JpaRepository<Doctor, String> {
+
+    /**
+     * Returns all the active doctors.
+     * 
+     * @param pageable the page information
+     * @return Page of Doctor
+     */
+    Page<Doctor> findAllByActiveTrue(Pageable pageable);
 
 }
